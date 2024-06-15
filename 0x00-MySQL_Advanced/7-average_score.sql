4f7c78b418224bdb7549da00ec9f366231a7cd75
AlexDelei <alexdelei125@gmail.com>
2024-06-15 20:45:19 +0000

    Calculating the average of scores and storing it using a procedure

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,16 +7,10 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
 	IN user_id INT
 )
 BEGIN
-	DECLARE avg_score DECIMAL(5, 2);
-
-	-- average score for a specific user
-        SELECT AVG(score) INTO avg_score
-	FROM corrections
-	WHERE user_id = user_id;
-
 	UPDATE users
-	SET average_score = avg_score
+	SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
         WHERE id = user_id;
 END$$
 
 DELIMITER ;
+
