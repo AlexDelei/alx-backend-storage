883eb11dab8fb855c5af100395431ec506693ec4
AlexDelei <alexdelei125@gmail.com>
2024-06-18 12:44:35 +0300

    Update content under the name

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Updating content with pymongo
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update my db
+
+    mongo_collection -- pymongo collection object
+    name -- school name to update
+    topics -- list of topics approached in the school
+    """
+    query_name = {name: name}
+    new_values = {"$set": topics}
+    mongo_collection.update_one(query_name, new_values)
+    return
