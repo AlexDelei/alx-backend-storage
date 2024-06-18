2dca0d0b68af320b875b32a0275ccaa52f055749
AlexDelei <alexdelei125@gmail.com>
2024-06-18 12:02:37 +0300

    insert a document in python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Inserting a document in pymongo
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Creating a collection with pymongo
+
+    mongo_collection -- pymongo collection object
+    kwargs -- key, value items. Its a dict
+    """
+    data = mongo_collection.insert_many(**kwargs)
+
+    return data.inserted_id
