9aaabcbf4a75a53b1ea294fcf8621fde446c7ed8
AlexDelei <alexdelei125@gmail.com>
2024-06-19 23:42:46 +0300

    Flush then generate and store

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,14 +4,14 @@ Writing strings to redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
     """
-    Set strings
+    Cache class to interact with Redis python API
     """
-    def __init__(self):
+    def __init__(self) -> None:
         """
         Initialising by flushing the database first
         """
@@ -20,8 +20,44 @@ class Cache:
 
     def store(self, data: Union[int, str, float, bytes]) -> str:
         """
-        Generating a uuid and use it as a key in setting up the values
+        Store the input data in Redis with a randomly generated UUID key
+
+        Args:
+            data (Union[int, str, float, bytes]): Data to be stored in Redis
+
+        Returns:
+            str: The UUID key under which the data is stored
         """
         id = uuid.uuid4()
         self._redis.set(str(id), data)
         return str(id)
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], Union[int, str, float, bytes]]]) -> Union[int, str, float, bytes, None]:
+        """
+        Retreives the data using the key
+
+        Args:
+            key (str): the object key to be used to retreive data.
+            fn (Callable): function that converts bytes data types to normal
+
+        Returns:
+            value: the value with their correct data types
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Coonverts bytes to string data type
+        """
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: int) -> Optional[int]:
+        """
+        Converting bytes to integer data type
+        """
+        return self.get(key, fn=int)
